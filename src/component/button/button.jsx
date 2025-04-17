31f050e2d2141d2f8fe6586020009da95a66ae81
Sandeep <your-email@example.com>
2025-04-17 13:09:32 +0530

    add new code

--- src/component/button/button.jsx
+++ src/component/button/button.jsx
@@ -8,12 +8,12 @@
     );
 }
  */
-export const Buttons = ({children , onSelect}) => {
-    return (
-        <>
-        <li className="btn">
-            <button onClick={onSelect}>{children}</button>    
-        </li>
-        </>
-    );
-}
+    export const Buttons = ({children , onSelect}) => {
+        return (
+            <>
+            <li className="btn">
+                <button onClick={onSelect}>{children}</button>    
+            </li>
+            </>
+        );
+    }
